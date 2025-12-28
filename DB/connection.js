c40a361c0f428c2d87696cd5823d76b500fd0f57
Bilawal-Rizvi <shahb7404@gmail.com>
2025-12-29 01:28:50 +0500

    Hello

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 mongoose
-  .connect("mongodb://localhost:27017/mydatabase", {
+  .connect(process.env.MONGODB_URI, {
    
   })
   .then(() => {
